09403f5eb4ae418f43bab4fb9987312bde760426
vinita1512 <vinitajambhale2000@gmail.com>
2026-03-23 23:16:16 +0530

    day 17: check number is even or odd

---
+++ checkEvenOdd.js
@@ -0,0 +1,17 @@
+// using if else
+
+function checkEvenOdd(n) {
+  if (n % 2 === 0) {
+    return n + " even";
+  } else {
+    return n + " odd";
+  }
+}
+console.log(checkEvenOdd(0));
+
+// using ternary operator
+
+// function checkEvenOdd(n) {
+//   return n % 2 === 0 ? `${n} is even` : `${n} is odd`;
+// }
+// console.log(checkEvenOdd(2));
